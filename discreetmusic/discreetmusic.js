4b4c57117d6b1cbd43a34a6506071034652bb4bb
Christopher Edgemon <chrisedgemon@gmail.com>
2017-06-05 17:01:55 -0400

    add discreetmusic/

---
+++ discreetmusic/discreetmusic.js
@@ -0,0 +1,11 @@
+let synth = new Tone.MonoSynth({
+    oscillator: {type: 'sawtooth'},
+    filterEnvelope: {
+    baseFrequency: 200,
+    octaves: 2
+    }
+});
+
+synth.toMaster();
+synth.triggerAttackRelease('C4', 1);
+
